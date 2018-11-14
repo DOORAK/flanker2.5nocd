3da6dabd7b3b142aa9ec68c809891a0fb8ffcbc9
DOORAK <DOORAK@users.noreply.github.com>
2018-11-14 15:21:51 +0300

    Update noCDflank.c

--- noCDflank.c
+++ noCDflank.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <windows.h>
-void _start(){ main();}
+FILE* f;
+const char magic[] = {0xb0,0x01,0x90,0x90,0x90};
+void _start() { main();}
 
-int main(){
-	FILE* f = fopen("flanker.exe","rb+");
-	if (f == NULL) { 
-						MessageBox(0,"NO flanker.exe here", "ERROR",0);
-						printf("ERRRRREOROOEOROOEOOROOEORORORERRRRRR!!!!!!!!!!");
-						return 0;
-					}
-	fseek(f,0x9239,0);
-	fputc(0xb0,f);
-	fputc(0x01,f);
-	fputc(0x90,f);
-	fputc(0x90,f);
-	fputc(0x90,f);
-	fclose(f);
-	return 0;
+int main() {
+	f = fopen("flanker.exe","rb+");
+	if (f != NULL) 	{ 			fseek(f,0x9239,0);
+						fwrite(&magic,5,1,f);
+						return 0;	}
+	
+	MessageBox(0,"NO flanker.exe here", "ERROR",0);
 }
